87e10ab364ca1fff18541dc8fcdedcdaffe91c57
Nikola Tasic <nikola4tasic@hotmail.com>
2019-11-05 00:59:40 +0100

    Finished the training method for DBN

--- DeepBeliefNetwork.py
+++ DeepBeliefNetwork.py
@@ -27,13 +27,44 @@ class DBN(torch.nn.Module):
         
         
     
-    def train(self, training_data, num_epochs):
+    def train(self, training_set, num_epochs):
         
+        new_training_set = training_set.copy()
         
         for i in range(num_hidden_layers):
             curr_rbm = Null
             if i == 0:
                 curr_rbm = RBM(self.num_visible, self.num_hidden[i], self.num_gibbs_samplings, self.learning_rate)
+                
+                curr_rbm.train(new_training_set, num_epochs)
+                
+                self.weights.append(curr_rbm.return_weights)  
+                self.RBMS.append(curr_rbm) 
+                
+                #after training a new RBM, we want to take its output as the new input for the next layer
+                for k in range(len(new_training_set)):
+                    visible_vector = new_training_set[k]
+                    _, sampled_hidden_vector = curr_rbm.sample_hidden(visible_vector)
+                        
+                    new_training_set[k] = sampled_hidden_vector
+                
             else:
                 curr_rbm = RBM(self.num_hidden[i-1], self.num_hidden[i], self.num_gibbs_samplings, self.learning_rate)
+                
+                #the training set is a list of input vectors
+                #we need to compute another list of vectors for additional hidden layers
+                curr_rbm.train(new_training_set, num_epochs)
+                
+                self.weights.append(curr_rbm.return_weights)  
+                self.RBMS.append(curr_rbm) 
+                
+                #after training a new RBM, we want to take its output as the new input for the next layer
+                for k in range(len(new_training_set)):
+                    visible_vector = new_training_set[k]
+                    _, sampled_hidden_vector = curr_rbm.sample_hidden(visible_vector)
+                        
+                    new_training_set[k] = sampled_hidden_vector
+                
+                
+            
             
